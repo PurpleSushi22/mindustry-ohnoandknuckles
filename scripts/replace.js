438df89b6dc6c9f19182f39f1e4ba2430ae931ca
PurpleSushi22 <105832169+PurpleSushi22@users.noreply.github.com>
2022-06-02 13:33:01 +0800

    i dunno

---
+++ scripts/replace.js
@@ -0,0 +1,5 @@
+var mymapd = SectorPreset("gigaFort", Planets.serpulo, 0);
+mymapd.alwaysUnlocked = true;
+mymapd.captureWave = 40;
+mymapd.difficulty = 2;
+mymapd.localizedName = "a map of all time";
