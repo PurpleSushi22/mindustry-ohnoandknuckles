30416e6925c7366aafb722602138d64344814e1d
PurpleSushi22 <105832169+PurpleSushi22@users.noreply.github.com>
2022-06-02 16:59:44 +0800

    ok???

--- scripts/replace.js
+++ scripts/replace.js
@@ -1,5 +1,5 @@
 var sector175 = SectorPreset("titaniumlol", Planets.serpulo, 175);
 sector175.alwaysUnlocked = true;
-sector175.waves = 40;
+sector175.captureWave = 30;
 sector175.difficulty = 2;
 sector175.localizedName = "the map of all time";
