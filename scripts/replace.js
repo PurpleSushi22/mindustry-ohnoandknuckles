3fd800d9457939df6235f2195bd1c06f0aab027d
PurpleSushi22 <105832169+PurpleSushi22@users.noreply.github.com>
2022-06-05 13:17:34 +0800

    why

--- scripts/replace.js
+++
@@ -1,5 +0,0 @@
-var sector175 = SectorPreset("titaniumlol", Planets.serpulo, 175);
-sector175.alwaysUnlocked = true;
-sector175.captureWave = 30;
-sector175.difficulty = 2;
-sector175.useAI = false;
